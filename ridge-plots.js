d568ac8ece0cd0c9e0b9aecc14eaa176c932dd40
Shudhanshu Gunjal <gunjalshudhanshu@gmail.com>
2022-02-15 22:26:21 +0530

    ridge-plots finished!

--- ridge-plots.js
+++ ridge-plots.js
@@ -3,91 +3,114 @@ function RidgePlots(){
 	this.panePARAMS = {
 		name: this.name,
 		arcSize: 400,
-        scaleFactor: 1.5
+        scaleFactor: 1.5,
+        maxRadius:500,
+        speed: 0.7,
+        lineGap: 30,
+        smallScale:5,
+        bigScale:50
+
 	}
     
     this.addPaneGui = function() {
         paneFolder = pane.addFolder({title: this.panePARAMS.name});
-        paneFolder.addInput(this.panePARAMS, 'arcSize', {
+        var arcShape = paneFolder.addFolder({ title: 'Arc Shape' })
+        var arcLines = paneFolder.addFolder({ title: 'Arc Lines' })
+        arcShape.addInput(this.panePARAMS, 'arcSize', {
             min: 200,
             max: 600,
           });
-          paneFolder.addInput(this.panePARAMS, 'scaleFactor', {
+        arcShape.addInput(this.panePARAMS, 'scaleFactor', {
             min: 1,
             max: 3,
           });
+
+        arcLines.addInput(this.panePARAMS, 'maxRadius', {
+            min: 100,
+            max: 1000,
+        });
+
+        arcLines.addInput(this.panePARAMS, 'speed', {
+            min: 0.1,
+            max: 2,
+        });
+
+        arcLines.addInput(this.panePARAMS, 'lineGap', {
+            min: 5,
+            max: 60,
+            step: 1
+        });
+
     }
 
     this.removePaneGui = function(){
         paneFolder.dispose();
     }
 
-
-	var startX = width/5;
-	var  endY = height/5;
-	var startY = height -endY
-	var spectrumWidth = (width/5)*3
-    var speed = 0.7;
-
+	var startX = width/2;
+	var startY = height/2
     var output = [];
-
-    // var fft = new p5.FFT();
     var fourier = new p5.FFT();
     
 this.addWave = function(){
     
     // output.push([{x: startX, y: startY}, {x: startX +spectrumWidth, y: startY}])
-	var w = fourier.waveform();
-    console.log(w)
+
+    var w = fourier.waveform();
+    var radius = this.panePARAMS.arcSize/2;
+
 	var outputWave = [];
-	var smallScale = 3; 2
-	var bigScale = 40;
 
-        for(var i = 0; i < w.length; i++) {
+        for(let i = 0; i < w.length; i++) {
 
-            if (i % 5 === 0) {
-                var y;
-                var x = map(i, 0, 1024, startX, startX + spectrumWidth)
-                if(i < 1024 * 0.25 || i > 1024 * 0.75) {
-                    y = map(w[i], - 1, 1, - smallScale, smallScale);
-                    outputWave.push({
-                        x: x,
-                        y: startY + y
-                    })
+            if (i % 20 === 0) {
+                var degree = map(i, 0, w.length, 0, 180);
+                var waveVal;
+
+                if(i < 1024 * 0.05 || i > 1024 * 0.95) {
+                    waveVal = map(w[i], - 1, 1, - this.panePARAMS.smallScale, this.panePARAMS.smallScale);
                 }
                 else {
-                    y = map(w[i], -1, 1, -bigScale, bigScale);
-                    outputWave.push({
-                        x: x,
-                        y: startY + y
-                    })
+                    waveVal = map(w[i], -1, 1, -this.panePARAMS.bigScale, this.panePARAMS.bigScale);
                 }
+
+                outputWave.push({
+                    radius: radius,
+                    degree: degree,
+                    waveVal: waveVal
+                })
             }
+
 	    }
 	    output.push(outputWave);
 
     }
 
 	this.draw = function(){
+        angleMode(DEGREES);
 		push();
         
         background(0)
         noFill();
         stroke(255)
         strokeWeight(2)
-        if(frameCount %30 === 0){
+        if(frameCount % this.panePARAMS.lineGap === 0){
             this.addWave();
         }
     
         for(var i= 0; i< output.length; i++){
             var o = output[i]
+            var degree= 0
             beginShape();
             for(var j = 0; j<o.length; j++){
-                o[j].y -= speed;
+                o[j].radius +=this.panePARAMS.speed
+                o[j].x = startX+(o[j].radius+o[j].waveVal)*cos(o[j].degree+180);
+                o[j].y = startY+(o[j].radius+o[j].waveVal)*sin(o[j].degree+180);
+                degree +=1
                 vertex(o[j].x, o[j].y);
             }
             endShape();
-            if(o[0].y < endY){
+            if(o[0].radius > this.panePARAMS.maxRadius){
                 output.splice(i,1);
             }
         }
@@ -97,10 +120,11 @@ this.addWave = function(){
         var mapBass = map( bass, 0, 255, 1, this.panePARAMS.scaleFactor);
 
         noStroke();
+        fill(255);
         arc(startX, startY,
             this.panePARAMS.arcSize*mapBass,
             this.panePARAMS.arcSize*mapBass,
-             PI, TWO_PI);
+             0, 180);
 
 		pop();
 	};
